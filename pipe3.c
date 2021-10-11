addc6d6d1d9326d7f87f6005039325b0cdf42bb5
Marc Espie <espie@hub.lse.epita.fr>
2021-10-11 17:02:56 +0200

    fluffy comments

--- pipe3.c
+++ pipe3.c
@@ -10,12 +10,18 @@
 
 #include "myfuncs.h"
 
+// this is how actual code usually works
+
+// message passing through pipes: we can pass binary data without issue
+// it's *more annoying* to debug than text data, but we don't have to
+// do *anything* to it (same box, same endianess, no alignment issue
 struct data {
 	int pid;
 	int result;
 };
 
-// this is how actual code usually works
+// if things are small enough, there won't be *any* interleaving
+// it's *guaranteed that small writes make it thru a pipe
 void
 perform_computation(int pid, int v, int fd)
 {
@@ -25,12 +31,19 @@ perform_computation(int pid, int v, int fd)
 	safe_write(fd, &d, sizeof d);
 }
 
+// so we send back our pid (one single pipe) to differentiate results
 struct myproc {
 	int pid;
 	int v;
 	int result;
 };
 
+// choice: for longer/more complex computations, we can associate one
+// pipe *per process*, but it becomes harder to figure out when things
+// happen
+
+
+// we generally need some simple way to associate pid to actual computation
 #define MYPROCS 10
 struct myproc allprocs[MYPROCS];
 
@@ -41,6 +54,7 @@ lookup(int pid)
 	for (i = 0; i != MYPROCS; i++)
 		if (allprocs[i].pid == pid)
 			return allprocs+i;
+	// should never happen
 	fprintf(stderr, "Fatal error: pid %d not found\n", pid);
 	exit(1);
 }
@@ -51,13 +65,13 @@ main()
 
 	int r, pip[2];
 	r = pipe(pip);
-	// mnemotechnic: stdin is fd 0, stdout is fd 1
-	// so pip[0] is the read side and pip[1] is the write side
 	if (r == -1)
 		err(1, "pipe");
 
 	int i;
+	// straightforward: start all computations, and record them
 	for (i = 0; i != MYPROCS; i++) {
+		struct myproc *p = allprocs + i;
 		int pid = fork();
 		int v = rand() % 200;
 		switch(pid) {
@@ -70,14 +84,17 @@ main()
 			perform_computation(getpid(), v, pip[0]);
 			exit(0);
 		}
-		allprocs[i].pid = pid;
-		allprocs[i].v = v;
-		allprocs[i].result = -1;
+		p->pid = pid;
+		p->v = v;
+		p->result = -1;
 	}
 	// father side
 	if (close(pip[0]) == -1)
 		err(1, "close");
 
+	// we actually get errors in two ways:
+	// - we read the results from pipe, and missing pid will stay
+	// with result == -1
 	while (true) {
 		struct data d;
 		ssize_t r = read(pip[1], &d, sizeof d);
@@ -92,6 +109,8 @@ main()
 	if (close(pip[1]) == -1)
 		err(1, "close");
 
+	// - or we get the error when we read the status from our dead child
+	// (this will most often match)
 	int rc = 0;
 	int status;
 	while ((r = wait(&status)) != -1) {
@@ -104,16 +123,17 @@ main()
 		err(1, "wait");
 
 	for (i = 0; i != MYPROCS; i++) {
-		if (allprocs[i].pid != -1) {
+		struct myproc *p = allprocs+i;
+		if (p->pid != -1) {
 			fprintf(stderr, "error: we did not reap %d\n",
-			    allprocs[i].pid);
+			    p->pid);
 		}
-		if (allprocs[i].result == -1) {
+		if (p->result == -1) {
 			fprintf(stderr, "error: no result for v=%d\n", 
-			    allprocs[i].v);
+			    p->v);
 		}
 		printf("Result for v=%d is %d\n",
-		    allprocs[i].v, allprocs[i].result);
+		    p->v, p->result);
 	}
 	exit(rc);
 }
