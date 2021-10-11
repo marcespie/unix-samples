672a5c5c1caf3be51ff6f492cc091b147763f61f
Marc Espie <espie@hub.lse.epita.fr>
2021-10-11 14:39:24 +0200

    sample code with fork

---
+++ fork1.c
@@ -0,0 +1,63 @@
+// always minimize includes, since C has one single namespace
+#include <unistd.h>
+#include <err.h>	// this is more or less standard these days
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void
+perform_computation(int i)
+{
+	printf("Square of %d is %d\n", i, i * i);
+}
+
+int
+main()
+{
+	int i = 15; // XXX implicit communication
+	// since fork() gives you a CC of the parent,
+	// -> you get implicit parameter passing to the child
+	int pid = fork();
+	switch(pid) {
+	case -1: 
+		err(1, "fork");
+	case 0:
+		perform_computation(i);
+		exit(0);
+	default:
+		break;
+	}
+	// this is purely the father's code
+	int status;
+	// always check *all* syscalls for errors
+	// always use waitpid *even if you have one single child*
+	// (you never know when you're going to reuse your code)
+	int r = waitpid(pid, &status, 0);
+	if (r == -1)
+		err(1, "waitpid");
+	// decode status
+	// some similar code is *required* each time you interact with
+	// children
+	if (WIFEXITED(status)) {
+		int rc = WEXITSTATUS(status);
+		if (rc == 0) {
+			// that case is 100% silent because 
+			exit(0);	// everything is okay
+		} else {
+			fprintf(stderr, "Child exited with exit(%d)\n", rc);
+			exit(1);
+		}
+	} else if (WIFSIGNALED(status)) {
+		int sig = WTERMSIG(status);
+		fprintf(stderr, "Child exited with signal %d (%s)\n", 
+		    sig, strsignal(sig));
+		exit(1);
+	} else {
+		// under normal circumstances, you either get a signal
+		// or an exit status
+		fprintf(stderr, "This should never happen: %d\n", status);
+		exit(1);
+	}
+}
+
