3c1371d2f6acc49d9825f342285eac4ad7cb3a47
Marc Espie <espie@hub.lse.epita.fr>
2021-10-12 15:23:45 +0200

    example with fdopen

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 ALLPROGS = fork1 fork2 fork3 fork3bad fork3bad2 fork4 fork5 fork6 \
-	pipe1 pipe2 pipe3 pipe4 pipe5 pipe5bad
+	pipe1 pipe1bis pipe2 pipe3 pipe4 pipe5 pipe5bad
 
 FORK6OBJS = myfuncs.o fork6.o
 PIPE1OBJS = myfuncs.o pipe1.o
+PIPE1BISOBJS = myfuncs.o pipe1bis.o
 PIPE2OBJS = myfuncs.o pipe2.o
 PIPE3OBJS = myfuncs.o pipe3.o
 PIPE5OBJS = myfuncs.o pipe5.o
@@ -12,6 +13,9 @@ all: ${ALLPROGS}
 
 pipe1: ${PIPE1OBJS}
 	${CC} -o $@ ${CFLAGS} ${PIPE1OBJS}
+
+pipe1bis: ${PIPE1BISOBJS}
+	${CC} -o $@ ${CFLAGS} ${PIPE1BISOBJS}
 
 pipe2: ${PIPE2OBJS}
 	${CC} -o $@ ${CFLAGS} ${PIPE2OBJS}
@@ -29,7 +33,7 @@ pipe5bad: ${PIPE5BADOBJS}
 	${CC} -o $@ ${CFLAGS} ${PIPE5BADOBJS}
 
 clean:
-	-rm -f ${ALLPROGS} ${FORK6OBJS} ${PIPE1OBJS} ${PIPE2OBJS} \
-	    ${PIPE3OBJS} ${PIPE5OBJS} ${PIPE5BADOBJS} 
+	-rm -f ${ALLPROGS} ${FORK6OBJS} ${PIPE1OBJS} ${PIPE1BISOBJS} \
+	    ${PIPE2OBJS} ${PIPE3OBJS} ${PIPE5OBJS} ${PIPE5BADOBJS} 
 
 .PHONY: all clean
---
+++ pipe1bis.c
@@ -0,0 +1,76 @@
+// the use of fdopen to get "nice" handles (caveats galore: you have
+// to be more or less CERTAIN your read/writes are small enough)
+#include <unistd.h>
+#include <err.h>	// this is more or less standard these days
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "myfuncs.h"
+
+void
+perform_computation(int i, FILE *f)
+{
+	char *res;
+
+	fprintf(f, "Square of %d is %d\n", i, i * i);
+	// XXX this is a pipe, we want to make sure the parent gets it
+	fflush(f); 
+}
+
+void
+file_copy(FILE *in, FILE *out)
+{
+	int c;
+	while (1) {
+		c = fgetc(in);
+		if (c == -1)
+			break;
+		fputc(c, out);
+	}
+	if (ferror(in))
+		errx(1, "Fatal error while reading from pipe");
+}
+
+int
+main()
+{
+	int i = 15; 
+
+	int rc, pip[2];
+	errwrap(rc = pipe(pip));
+	int pid;
+
+	errwrap(pid = fork());
+
+	if (pid == 0) {
+		errwrap(close(pip[1]));
+		FILE *f = fdopen(pip[0], "w");
+		// XXX the libc often defines errno stuff for "non syscalls"
+		if (!f)
+			err(1, "fdopen");
+		perform_computation(i, f);
+		exit(0);
+	}
+	// father
+	errwrap(close(pip[0]));
+
+	FILE *in = fdopen(pip[1], "r");
+	if (!in)
+		err(1, "fdopen");
+
+	file_copy(in, stdout);
+
+	// and it also "transparently" passes thru errno from syscalls
+	// (fclose calls close)
+	if (fclose(in) != 0)
+		err(1, "fclose");
+
+	int status, r;
+
+	errwrap(r = waitpid(pid, &status, 0));
+	exit(bad_status(status, pid) ? 1 : 0);
+}
+
