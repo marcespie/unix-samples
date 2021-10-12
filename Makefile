0a6ffc2069a4ccc86e708716eaa5f6985c7696e3
Marc Espie <espie@hub.lse.epita.fr>
2021-10-12 15:07:38 +0200

    better pipe2

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ ALLPROGS = fork1 fork2 fork3 fork3bad fork3bad2 fork4 fork5 fork6 \
 
 FORK6OBJS = myfuncs.o fork6.o
 PIPE1OBJS = myfuncs.o pipe1.o
-PIPE3OBJS = myfuncs.o pipe3.o
+PIPE2OBJS = myfuncs.o pipe2.o
 PIPE4OBJS = myfuncs.o pipe4.o
 PIPE5OBJS = myfuncs.o pipe5.o
 PIPE5BADOBJS = myfuncs.o pipe5bad.o
@@ -13,8 +13,8 @@ all: ${ALLPROGS}
 pipe1: ${PIPE1OBJS}
 	${CC} -o $@ ${CFLAGS} ${PIPE1OBJS}
 
-pipe3: ${PIPE3OBJS}
-	${CC} -o $@ ${CFLAGS} ${PIPE3OBJS}
+pipe2: ${PIPE2OBJS}
+	${CC} -o $@ ${CFLAGS} ${PIPE2OBJS}
 
 pipe4: ${PIPE4OBJS}
 	${CC} -o $@ ${CFLAGS} ${PIPE4OBJS}
@@ -29,7 +29,7 @@ pipe5bad: ${PIPE5BADOBJS}
 	${CC} -o $@ ${CFLAGS} ${PIPE5BADOBJS}
 
 clean:
-	-rm -f ${ALLPROGS} ${FORK6OBJS} ${PIPE1OBJS} ${PIPE3OBJS} \
+	-rm -f ${ALLPROGS} ${FORK6OBJS} ${PIPE1OBJS} ${PIPE2OBJS} \
 	    ${PIPE4OBJS} ${PIPE5OBJS} ${PIPE5BADOBJS} 
 
 .PHONY: all clean
--- pipe2.c
+++ pipe2.c
@@ -1,125 +1,140 @@
-// always minimize includes, since C has one single namespace
+// actually send structured data back
 #include <unistd.h>
-#include <err.h>	// this is more or less standard these days
+#include <err.h>	
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <errno.h>
 
+#include "myfuncs.h"
+
+// we're getting closer to actual cod3
+
+// message passing through pipes: we can pass binary data without issue
+// it's *more annoying* to debug than text data, but we don't have to
+// do *anything* to it (same box, same endianess, no alignment issue)
+
+// we send back our pid (one single pipe) to differentiate results
+struct data {
+	int pid;
+	int result;
+};
+
+// if things are small enough, there won't be *any* interleaving
+// it's *guaranteed* that small writes make it thru a pipe
 void
-safe_write(int fd, 
-    const void *buf, // ALWAYS annotate your functions to be const correct
-    size_t sz)
+perform_computation(int pid, int v, int fd)
 {
-	// XXX stdlib wants void *, but we need char* for pointer
-	// arithmetic
-	const char *b = buf;
-	while (sz != 0) {
-		ssize_t r = write(fd, b, sz);
-		if (r == -1)
-			err(1, "write");
-		assert(r <= sz);
-		b += r;
-		sz -= r;
-		// loop invariant: we have to write [buf, buf+sz[ to fd
-	}
-	// loop termination: sz becomes zero eventually
-	// write will *never* returns 0
+	struct data d;
+	d.pid = pid;
+	d.result = v * v;
+	// safe_write is not strictly necessary, but hey belt & suspendwer
+	safe_write(fd, &d, sizeof d);
 }
 
-void
-perform_computation(int i, int fd)
-{
-	char *res;
 
-	asprintf(&res, "Square of %d is %d\n", i, i * i);
-	safe_write(fd, res, strlen(res));
-}
+// choice: for longer/more complex computations, we can associate one
+// pipe *per process*, but we have to either poll the pipes, OR get the
+// result from each child sequentially
+// one single pipe + demultiplexing means we get data easily
 
-// lift the handling code into a separate function
-void 
-decode_status(int status)
+
+// systems programming involves data structures and algorithms:
+// we need some kind of registry per-pid to remember what child is handling
+// which computation
+struct myproc {
+	int pid;
+	int v;
+	int result;
+};
+
+#define MYPROCS 10
+struct myproc allprocs[MYPROCS];
+
+struct myproc * 
+lookup(int pid)
 {
-	if (WIFEXITED(status)) {
-		int rc = WEXITSTATUS(status);
-		if (rc == 0)
-			return;
-		else {
-			fprintf(stderr, "Child exited with exit(%d)\n", rc);
-			exit(1);
-		}
-	} else if (WIFSIGNALED(status)) {
-		int sig = WTERMSIG(status);
-		fprintf(stderr, "Child exited with signal %d (%s)\n", 
-		    sig, strsignal(sig));
-	} else {
-		// under normal circumstances, you either get a signal
-		// or an exit status
-		fprintf(stderr, "This should never happen: %d\n", status);
-	}
+	int i;
+	for (i = 0; i != MYPROCS; i++)
+		if (allprocs[i].pid == pid)
+			return allprocs+i;
+	// should never happen
+	fprintf(stderr, "Fatal error: pid %d not found\n", pid);
 	exit(1);
-}
-
-#define MYSIZE 1024
-void
-print_from(int fd)
-{
-	char buf[MYSIZE];
-	while (1) {
-		ssize_t r = read(fd, buf, MYSIZE);
-		if (r == -1)
-			err(1, "read");
-		if (r == 0)
-			break;
-		safe_write(1, buf, r);
-	}
 }
 
 int
 main()
 {
-	int i = 15; // XXX implicit communication
-	// since fork() gives you a CC of the parent,
-	// -> you get implicit parameter passing to the child
-	// explicit communication from child to father
 
-	int rc, pip[2];
-	rc = pipe(pip);
-	// mnemotechnic: stdin is fd 0, stdout is fd 1
-	// so pip[0] is the read side and pip[1] is the write side
-	if (rc == -1)
-		err(1, "pipe");
-	int pid = fork();
-	switch(pid) {
-	case -1: 
-		err(1, "fork");
-	case 0:
-		// XXX ALWAYS close the side of the pipe you don't want
-		if (close(pip[1]) == -1)
-			err(1, "close");
-		perform_computation(i, pip[0]);
-		exit(0);
-	default:
-		if (close(pip[0]) == -1)
-			err(1, "close");
-		break;
+	int pip[2];
+	errwrap(pipe(pip));
+
+	int i;
+	// straightforward: start all computations, and record them
+	for (i = 0; i != MYPROCS; i++) {
+		struct myproc *p = allprocs + i;
+		int v = rand() % 200;
+		int pid;
+		errwrap(pid = fork());
+		if (pid == 0) {
+			errwrap(close(pip[1]));
+			perform_computation(getpid(), v, pip[0]);
+			exit(0);
+		}
+		// father
+		p->pid = pid;
+		p->v = v;
+		p->result = -1;
 	}
-	// get the result and print it
-	print_from(pip[1]);
-	if (close(pip[1]) == -1)
-		err(1, "close");
+	errwrap(close(pip[0]));
 
-	// this is purely the father's code
-	int status;
-	// always check *all* syscalls for errors
-	// always use waitpid *even if you have one single child*
-	// (you never know when you're going to reuse your code)
-	int r = waitpid(pid, &status, 0);
-	if (r == -1)
-		err(1, "waitpid");
-	// decode status
-	decode_status(status);
-	exit(0);
+	// we actually get errors in two ways:
+	// - we read the results from pipe, and missing pid will stay
+	// with result == -1
+	while (true) {
+		struct data d;
+		ssize_t r;
+		errwrap(r = read(pip[1], &d, sizeof d));
+		if (r == 0) // it's often simpler to code "exit loop in
+			    // the middle" rather than add arbitrary bool
+			break;
+		// match pipe data to required computation
+		struct myproc *p = lookup(d.pid);
+		p->result = d.result;
+	}
+
+	errwrap(close(pip[1]));
+	// - or we get the error when we read the status from our dead child
+	// (note that exit from child + no result is the "normal" case
+	int rc = 0;
+	int status, pid;
+	while ((pid = wait(&status)) != -1) {
+		if (bad_status(status, pid))
+			rc = 1;
+		struct myproc *p = lookup(pid);
+		p->pid = -1;
+	}
+	if (errno != ECHILD)
+		err(1, "wait");
+
+	// new part: check what part of our data structure is still unfilled
+	for (i = 0; i != MYPROCS; i++) {
+		struct myproc *p = allprocs+i;
+		if (p->pid != -1) {
+			fprintf(stderr, "error: we did not reap %d\n",
+			    p->pid);
+		}
+		if (p->result == -1) {
+			fprintf(stderr, "error: no result for v=%d\n", 
+			    p->v);
+		}
+		printf("Result for v=%d is %d\n",
+		    p->v, p->result);
+	}
+	exit(rc);
 }
 
